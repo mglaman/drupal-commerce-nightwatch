2ee61c364d91c4e1fff924958d1ab6c54a52e6e9
Daniel Wehner <dawehner@googlemail.com>
2018-02-20 21:36:51 +0000

    Start a webserver automatically, when no base_url got defined before

--- core/tests/Drupal/Nightwatch/globals.js
+++ core/tests/Drupal/Nightwatch/globals.js
@@ -1,9 +1,23 @@
-const chromedriver = require('chromedriver');
+import { spawn } from 'child_process';
+import chromedriver from 'chromedriver';
+
+const runAsWebserverIfAvailable = (command) => {
+  if (process.env.WEBSERVER_USER) {
+    return `sudo -u ${process.env.WEBSERVER_USER} ${command}`;
+  }
+  return command;
+};
 
 module.exports = {
   before: (done) => {
     if (!JSON.parse(process.env.CHROME_STANDALONE)) {
       chromedriver.start();
+    }
+    // Automatically start a webserver.
+    if (!process.env.BASE_URL) {
+      // @todo Use https://www.drupal.org/project/ideas/issues/2911319 once its available.
+      process.env.BASE_URL = 'http://localhost:8888';
+      spawn(runAsWebserverIfAvailable('php'), ['-S', 'localhost:8888', '-t', '../', '.ht.router.php']);
     }
     done();
   },
@@ -13,4 +27,5 @@ module.exports = {
     }
     done();
   },
+  runAsWebserverIfAvailable,
 };
