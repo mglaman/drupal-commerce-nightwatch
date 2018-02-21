4c19bd98a6b2de86776bf6b562854d839f3898ca
Alex Pott <alex.a.pott@googlemail.com>
2018-02-21 09:48:26 +0000

    Improve help

--- core/tests/Drupal/Nightwatch/Commands/installDrupal.js
+++ core/tests/Drupal/Nightwatch/Commands/installDrupal.js
@@ -4,20 +4,20 @@ import { commandAsWebserver } from '../globals';
 exports.command = function installDrupal(setupClass = '', callback) {
   const self = this;
 
-  const dbOption = process.env.DB_URL.length > 0 ? `--db_url ${process.env.DB_URL}` : '';
+  const dbOption = process.env.DRUPAL_DB_URL.length > 0 ? `--db_url ${process.env.DRUPAL_DB_URL}` : '';
   let dbPrefix = '';
 
   try {
     // Single slash is replaced with 2 slashes because it will get printed on the command line,
     // which will be escaped again by the PHP script.
-    const install = execSync(commandAsWebserver(`php ./scripts/test-site.php install --setup_class ${setupClass.replace(/\\/g, '\\\\')} --base_url ${process.env.BASE_URL} ${dbOption} --json`));
+    const install = execSync(commandAsWebserver(`php ./scripts/test-site.php install --setup_class ${setupClass.replace(/\\/g, '\\\\')} --base_url ${process.env.DRUPAL_BASE_URL} ${dbOption} --json`));
     const installData = JSON.parse(install.toString());
     dbPrefix = installData.db_prefix;
-    const matches = process.env.BASE_URL.match(/^https?\:\/\/([^\/?#]+)(?:[\/?#]|$)/i);
+    const matches = process.env.DRUPAL_BASE_URL.match(/^https?\:\/\/([^\/?#]+)(?:[\/?#]|$)/i);
     const domain = matches[1];
     const path = matches[2];
     this
-      .url(process.env.BASE_URL)
+      .url(process.env.DRUPAL_BASE_URL)
       .setCookie({
         name: 'SIMPLETEST_USER_AGENT',
         // Colons needs to be URL encoded to be valid.
