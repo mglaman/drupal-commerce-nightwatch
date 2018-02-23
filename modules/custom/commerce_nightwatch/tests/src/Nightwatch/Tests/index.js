78c66038bfb150c2a246a21933cbd69d09575953
Matt Glaman <nmd.matt@gmail.com>
2018-02-23 00:21:08 -0600

    Try to fix errors

--- modules/custom/commerce_nightwatch/tests/src/Nightwatch/Tests/index.js
+++ modules/custom/commerce_nightwatch/tests/src/Nightwatch/Tests/index.js
@@ -15,24 +15,22 @@ module.exports = {
     browser
       .relativeURL('/product/1')
       .waitForElementVisible('body', 1000)
-      .assert.containsText('body', 'My product');
-    browser.click('input[name=op]');
+      .assert.containsText('body', 'My product')
+      .useCss().click('input[name=op]');
     browser
       .waitForElementVisible('body', 1000)
       .assert.containsText('body', 'My product added to your cart.')
-      .useXpath().click('//a[text()="your cart"]').useCss();
+      .useXpath().click('//a[text()="your cart"]');
     browser
       .waitForElementVisible('body', 1000)
-      .assert.containsText('body', 'Shopping cart');
-    browser.click('input[name=op]');
+      .assert.containsText('body', 'Shopping cart')
+      .useCss().click('input[name=op]');
     browser
       .waitForElementVisible('body', 1000)
-      .assert.containsText('body', 'Returning Customer');
-    browser
-      .assert.containsText('body', 'Guest Checkout');
-    browser
-      .assert.containsText('body', 'Proceed to checkout. You can optionally create an account at the end.');
-    browser.click('input[value="Continue as Guest"]');
+      .assert.containsText('body', 'Returning Customer')
+      .assert.containsText('body', 'Guest Checkout')
+      .assert.containsText('body', 'Proceed to checkout. You can optionally create an account at the end.')
+      .useCss().click('input[value="Continue as Guest"]');
 
     browser.setValue('input[name="contact_information[email]"]', 'myemail@example.com');
     browser.setValue('input[name="contact_information[email_confirm]"]', 'myemail@example.com');
